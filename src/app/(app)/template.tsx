add70c3f538d37f3677240b3f02769427864e58f
Abdoulaye Etienne samake <etiennesamake@gmail.com>
2026-07-20 17:57:52 +0000

    fix: changer h-full en min-h-full dans template.tsx pour permettre le défilement de la page de création de facture

--- src/app/(app)/template.tsx
+++ src/app/(app)/template.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from 'react';
 
 export default function AppTemplate({ children }: { children: ReactNode }) {
   return (
-    <div className="animate-in fade-in slide-in-from-bottom-4 duration-500 ease-out h-full">
+    <div className="animate-in fade-in slide-in-from-bottom-4 duration-500 ease-out min-h-full">
       {children}
     </div>
   );
